e81f8fcdc3706ab8ac55ce80537a29c0ce0aa396
Jacob Mziya <jacobmziya@gmail.com>
2016-11-21 11:53:00 +0200

    Getting dashboard with couch.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
     average_trends = []
     treatments = []
 
-    Site.by_status.key('Success').each do|site|
+    Site.by_name.each do|site|
 
-      site_data = site#PullTracker.find_by_sql("SELECT * FROM pull_trackers
+      site_data = PullTracker.by_status.key('Success').first #PullTracker.find_by_sql("SELECT * FROM pull_trackers
                     #WHERE site_id = #{site.id} AND status = 'Success' ORDER BY id DESC LIMIT 1").last
       next if site_data.blank?
       data[site.name] = {}
