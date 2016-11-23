324ce6eb2150206af06f3f5f2c1435a07fbdf756
Jacob Mziya <jacobmziya@gmail.com>
2016-11-23 08:26:03 +0200

    Completed working on mysqltocouch. AutoGetting Data remaining.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,25 +12,21 @@ class HomeController < ApplicationController
     sites = Site.by_name.each
 
     sites.each do|site|
-
-      #site_data = PullTracker.by_status.key('Success').first #PullTracker.find_by_sql("SELECT * FROM pull_trackers
-                    #WHERE site_id = #{site.id} AND status = 'Success' ORDER BY id DESC LIMIT 1").last
-      site_data = JSON.parse(site.data) rescue nil
+      site_data = eval(site.data)
       next if site_data.blank?
       data[site.name] = {}
-      #Rails.logger.debug JSON.parse(site_data).inspect
       data[site.name]["data"] = site_data
-
       data[site.name]["site_code"] = site.site_code
-      data[site.name]["date"] = site_data.date.to_date.to_s
-      data[site.name]['obsolete_today'] = (Date.today >  site_data.date.to_date) ? true : false
-      data[site.name]['obsolete_month'] = (Date.today.beginning_of_month <=  site_data.date.to_date &&  site_data.date.to_date <= Date.today.end_of_month) ? false : true
-      data[site.name]['obsolete_quarter'] = (Date.today.beginning_of_quarter <=  site_data.date.to_date &&  site_data.date.to_date <= Date.today.end_of_quarter) ? false : true
-      data[site.name]['obsolete_year'] = (Date.today.beginning_of_year <=  site_data.date.to_date &&  site_data.date.to_date <= Date.today.end_of_year) ? false : true
+      data[site.name]["date"] = site.date.to_date.to_s
+      data[site.name]['obsolete_today'] = (Date.today >  site.date.to_date) ? true : false
+      data[site.name]['obsolete_month'] = (Date.today.beginning_of_month <=  site.date.to_date &&  site.date.to_date <= Date.today.end_of_month) ? false : true
+      data[site.name]['obsolete_quarter'] = (Date.today.beginning_of_quarter <=  site.date.to_date &&  site.date.to_date <= Date.today.end_of_quarter) ? false : true
+      data[site.name]['obsolete_year'] = (Date.today.beginning_of_year <=  site.date.to_date &&  site.date.to_date <= Date.today.end_of_year) ? false : true
 
 
       treatments << data[site.name]["data"]["dispensation_trends"] rescue []
     end
+
 
     if treatments.length > 0
       months = treatments.first
